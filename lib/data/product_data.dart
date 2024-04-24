13b9e7e210e414d383d0d8acba92b15b57014221
ANGEL_OMAR_GUERRERO_ORTEGA <a.21308051280366@cbtis128.edu.mx>
2024-04-24 14:47:36 +0000

    act

---
+++ lib/data/product_data.dart
@@ -0,0 +1,16 @@
+import 'package:guerrero0366/models/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/logo.png",
+      title: "Mercado Libre",
+      descripcion: "Esta Es La Mejor Pagina De Compras"),
+  ProductModel(
+      img: "assets/images/logo2.png",
+      title: "Nuestro Logo",
+      descripcion: "Echo En Casa"),
+  ProductModel(
+      img: "assets/images/logo3.png",
+      title: "Nuestras Paginas",
+      descripcion: "De Mercado Libre Para Ti")
+];
---
+++ lib/models/product_model.dart
@@ -0,0 +1,8 @@
+class ProductModel {
+  final String img;
+  final String title;
+  final String descripcion;
+
+  ProductModel(
+      {required this.img, required this.title, required this.descripcion});
+}
